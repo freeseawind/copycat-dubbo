8f82b16e0dc673fe0bd84409caf22cfcdd5e5514
freeseawind <iphonewang@foxmail.com>
2018-09-16 23:01:09 +0800

    集群容错功能测试

--- copycat-dubbo/copycat-dubbo-example/src/main/java/github/freeseawind/springboot/helloworld/consumer/ConsumerTest.java
+++ copycat-dubbo/copycat-dubbo-example/src/main/java/github/freeseawind/springboot/helloworld/consumer/ConsumerTest.java
@@ -24,20 +24,31 @@ import github.freeseawind.service.DemoService;
 @SpringBootTest(classes = ConsumerTest.class)
 public class ConsumerTest
 {
-    @Reference(loadbalance="random")
-    private DemoService demoService;
+    @Reference(loadbalance = "random", cluster = "failfast", retries = 2)
+    private DemoService failFastService;
+    
+    @Reference(loadbalance = "random", cluster = "failover", retries = 2)
+    private DemoService failOverService;
     
     @Test
     public void testRPC()
     {
         for(int i = 0; i < 100; i++)
         {
-            System.out.println(demoService.sayHello("freeseawind"));
+            System.out.println(failFastService.sayHello("freeseawind"));
         }
         
-        String str = demoService.sayHello("freeseawind");
+        String str = failFastService.sayHello("freeseawind");
         
         assertEquals("Hello freeseawind", str);
         
+    }
+    
+    @Test
+    public void testFail()
+    {
+        String str = failFastService.sayHello("freeseawind");
+        
+        System.out.println(str);
     }
 }
